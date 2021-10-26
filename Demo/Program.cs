fe616dce4b137f1ffc2812170f9c33fa09818038
rupeshsingh.ags@gmail.com <rupeshsingh.ags@gmail.com>
2021-10-26 18:46:57 +0530

    calculation code push

---
+++ Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Demo
+{
+   public class Program
+    {
+       
+        public static int isSum(int a,int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        
+       public static void Main(string[] args)
+       {
+            
+            Console.WriteLine("A+B=C value is:" + Program.isSum(20, 30));
+            Console.WriteLine();
+        }
+    }
+}
