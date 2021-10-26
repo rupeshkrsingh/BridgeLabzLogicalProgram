ad919b0ac3b672157a384be009f11c5ce99be00d
rupeshsingh.ags@gmail.com <rupeshsingh.ags@gmail.com>
2021-10-26 18:12:31 +0530

    codecommit

---
+++ BridgeLabzLogicalProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BridgeLabzLogicalProgram
+{
+   public class Program
+    {
+      public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is rupesh");
+            Console.ReadKey();
+        }
+    }
+}
